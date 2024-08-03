1195e5c35c73f4f1170c2d47576a23869b9f910c
shubham <shubham.devops.cloud@gmail.com>
2024-08-03 17:13:12 +0530

    update

--- vars/helmDeployment.groovy
+++ vars/helmDeployment.groovy
@@ -3,6 +3,7 @@ def call(body){
     def repoName = REPONAME
     def featureimage = FEATUREIMAGE
     def envconfigTag = NAMESPACE
+    def dockerImagePath = "charts/docker.app.yaml"
 
     node("worker_docker_slave"){
         properties([
@@ -30,12 +31,15 @@ def call(body){
 
             stage("Intialise workspace"){
                 println "$repoName $featureimage $envconfigTag"
-                sh """
-                    ls -la
-                    cat charts/docker.app.yaml
-                    helm version
-                    kubectl version --client
-                """
+
+                if (${featureimage} == null){
+                    println "featureimage not passed while running build so failing this build"
+                    throw new RuntimeException("Sonarqube check has failed, this component is under threshold")
+                }
+
+                sh "cat charts/docker.app.yaml"
+                sh "sed -i '/DOCKER_APP_IMAGE:/c DOCKER_APP_IMAGE: $featureimage' $dockerImagePath"
+                sh "cat charts/docker.app.yaml"
             }
         }
     }
