fccfce186bf09b18b5c267dd68e180d008c4e410
shubham <shubham.devops.cloud@gmail.com>
2024-08-04 16:08:54 +0530

    update

--- vars/helmDeployment.groovy
+++ vars/helmDeployment.groovy
@@ -50,9 +50,7 @@ def call(body){
 
             // stage('Deploying application on k8s'){
             //     withCredentials([kubeconfigContent(credentialsId: 'KUBE-CONFIG', variable: 'KUBECONFIG_CONTENT')]) {
-            //         dir("charts"){
-            //             sh "helm upgrade --install --namespace ${envcongTag} example --debug --timeout 900s --wait" 
-            //         }
+            //         sh "helm upgrade --install -n ${envconfigTag} ${repoName} ./charts --debug --timeout 900s --wait" 
             //     }
             // }
         }
